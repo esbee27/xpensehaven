292bec8aa1982cbe9060be1d5dc825cc50d1bcad
Maryam <morrryyyy21@gmail.com>
2024-12-28 02:21:45 +0100

    create footer for lanfing page

---
+++ src/features/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-logo">
+        Xpense<span className="footer-color">Haven</span>
+      </p>
+      <p className="footer-copyright">&copy; 2024 XpenseHaven.</p>
+    </footer>
+  );
+};
+
+export default Footer;
