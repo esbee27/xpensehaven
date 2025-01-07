c8d28eb01606eaec7e90126177484012a75a39aa
Maryam <morrryyyy21@gmail.com>
2025-01-07 08:36:21 +0100

    add structures for authentication pages

---
+++ src/pages/login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./auth.css"; // Shared styles for Login and Signup
+import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
+
+const Login = () => {
+  return (
+    <div className="auth-container">
+      <Logo />
+      <h1 className="auth-title">Login</h1>
+      <form className="auth-form">
+        <input
+          type="text"
+          placeholder="Enter Username or Email"
+          className="auth-input"
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          className="auth-input"
+        />
+        <button type="submit" className="auth-button primary">
+          Sign in
+        </button>
+        <div className="auth-divider">
+          <span>or</span>
+        </div>
+        <button type="button" className="auth-button google">
+          <img
+            src="src/assets/images/google-icon.png"
+            alt="Google"
+            className="google-icon"
+          />
+          Sign in with Google
+        </button>
+      </form>
+      <p className="auth-footer">
+        Don't have an account? <Link to="/signup">Signup</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/signup.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./auth.css"; // Shared styles for Login and Signup
+import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
+
+const Signup = () => (
+  <div className="auth-container">
+    <Logo />
+    <h1 className="auth-title">Sign Up</h1>
+    <form className="auth-form">
+      <input type="text" placeholder="Enter Username" className="auth-input" />
+      <input type="email" placeholder="Enter Email" className="auth-input" />
+      <input
+        type="password"
+        placeholder="Enter Password"
+        className="auth-input"
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        className="auth-input"
+      />
+      <button type="submit" className="auth-button primary">
+        Sign Up with Email
+      </button>
+      <div className="auth-divider">
+        <span>or</span>
+      </div>
+      <button type="button" className="auth-button google">
+        <img
+          src="/assets/images/google-icon.png"
+          alt="Google"
+          className="google-icon"
+        />
+        Sign up with Google
+      </button>
+    </form>
+    <p className="auth-footer">
+      Already have an account? <Link to="/login">Login</Link>
+    </p>
+  </div>
+);
+
+export default Signup;
