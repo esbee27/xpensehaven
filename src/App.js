2ef42d2056beb9a885e5a91c2f7230be75402510
Maryam <morrryyyy21@gmail.com>
2025-01-15 21:46:19 +0100

    add transaction, budget, and authentication pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import About from "./pages/about"; //  About page
+import { CurrencyProvider } from "./context/currencyContext"; // Import CurrencyProvider to manage global currency state
+import LandingPage from "./pages/LandingPage"; // Import Landing page
+import About from "./pages/AboutPage"; // Import About page
+import DashboardPage from "./pages/DashboardPages/MainDashboard"; // Import Dashboard page component
+import TransactionPage from "./pages/TransactionPage";
+import SettingsPage from "./pages/DashboardPages/SettingsPages/Settings"; // Import SettingsPage
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   return (
-    <Router>
-      <div>
-        {/* Define the routes */}
-        <Routes>
-          {/* The LandingPage component is displayed for the root path */}
-          <Route path="/" element={<LandingPage />} />
+    <CurrencyProvider>
+      {" "}
+      {/* Wrap the app with CurrencyProvider to make the currency state accessible */}
+      <Router>
+        <div>
+          {/* Define the routes */}
+          <Routes>
+            {/* The LandingPage component is displayed for the root path */}
+            <Route path="/" element={<LandingPage />} />
 
-          {/* The About component is displayed for the /about path */}
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+            {/* The About component is displayed for the /about path */}
+            <Route path="/about" element={<About />} />
+
+            {/* The Dashboard component is displayed for the /dashboard path */}
+            <Route path="/dashboard" element={<DashboardPage />} />
+
+            {/* The Dashboard component is displayed for the /trnsaction page path */}
+            <Route path="/transactions" element={<TransactionPage />} />
+
+            {/* The Settings component is displayed for the /settings path */}
+            <Route path="/settings" element={<SettingsPage />} />
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </CurrencyProvider>
   );
 }
 
