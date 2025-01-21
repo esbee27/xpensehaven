f63f7efadfee70582384dedb5c37035fa6823f06
Maryam <morrryyyy21@gmail.com>
2025-01-21 21:06:31 +0100

    import auth context

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,27 +1,48 @@
-import React from "react";
-import "./auth.css"; // Shared styles for Login and Signup
+// src/pages/login.js
+import React, { useState } from "react";
+import "./auth.css";
 import { Link } from "react-router-dom";
 import Logo from "../components/Logo";
 import googleIcon from "../assets/images/google-icon.png";
+import { useAuth } from "../context/AuthContext"; // Import useAuth to access login function
 
 const Login = () => {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const { handleLogin, loading, error } = useAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(identifier, password);
+  };
+
   return (
     <div className="auth-container">
       <Logo />
       <h1 className="auth-title">Login</h1>
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter Username or Email"
           className="auth-input"
+          value={identifier}
+          onChange={(e) => setIdentifier(e.target.value)}
         />
         <input
           type="password"
           placeholder="Enter Password"
           className="auth-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit" className="auth-button primary">
-          Sign in
+        {error && <p className="auth-error">{error}</p>}{" "}
+        {/* Display error message */}
+        <button
+          type="submit"
+          className="auth-button primary"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign in"}
         </button>
         <div className="auth-divider">
           <span>or</span>
