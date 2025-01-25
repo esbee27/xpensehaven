363b933b259147b1ae8be86e9c5bdb4e09768181
Maryam <morrryyyy21@gmail.com>
2025-01-25 11:48:41 +0100

    create the call to action section of the landing page

---
+++ src/features/CTA/CTA.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./CTA.css";
+
+function CTA() {
+  return (
+    <section className="cta-section">
+      <h2 className="cta-title">Take Control of Your Expenses Today</h2>
+      <p className="cta-text">
+        Join thousands of users simplifying their financial tracking with
+        fyenance.
+      </p>
+      <button className="cta-button">Get Started</button>
+    </section>
+  );
+}
+
+export default CTA;
