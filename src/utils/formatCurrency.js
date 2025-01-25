a6607e0a1c58ac7f8cb0b4d1e57dda8ec7a66e6c
Maryam <morrryyyy21@gmail.com>
2025-01-25 21:36:09 +0100

    add styling for the main dashboard page

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,10 +1,10 @@
 // src/utils/formatCurrency.js
-export const formatCurrency = (amount, currency) => {
-    const formattedAmount = new Intl.NumberFormat("en-NG", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    }).format(amount);
-  
-    return `${currency}${formattedAmount}`;
-  };
-  
+export const formatCurrency = (amount, currency, showDecimals = true) => {
+  const formattedAmount = new Intl.NumberFormat("en-NG", {
+    minimumFractionDigits: showDecimals ? 2 : 0,
+    maximumFractionDigits: showDecimals ? 2 : 0,
+  }).format(amount);
+
+  return `${currency}${formattedAmount}`;
+};
+
