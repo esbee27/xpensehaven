bfe460a20f8b870406e947c05e07e8af836629f1
Maryam <morrryyyy21@gmail.com>
2025-01-25 11:40:51 +0100

    delete react files

--- src/components/DashboardComponents/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as DashboardNav } from "./DashboardNav";
-export { default as ActionLinks } from "./ActionLinks"
-export { default as Greeting } from "./Greeting";
-export { default as NewUserPlaceholder } from "./NewUserPlaceholder";
